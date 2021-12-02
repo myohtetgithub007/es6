ee40b90f82c1826c460dafefdf92315e139e245e
Myo Htet <myohtetcu2021@gmail.com>
2021-12-02 18:29:01 +0900

    map function

---
+++ app.js
@@ -0,0 +1,10 @@
+function map(){
+	let num = [1,2,3,4,5];
+	let result = num.map(function(n){
+		return n + 1;
+	});
+	console.log(result);
+}
+/* map function how is work
+
+copy array crate new array eg ((result array)) is new
