6b829c63d61a638ef04b6aacbad82d01ecb01280
Myo Htet <myohtetcu2021@gmail.com>
2021-12-06 18:46:35 +0900

    es6 filter

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-function map(){
-	let num = [1,2,3,4,5];
-	let result = num.map(function(n){
-		return n + 1;
-	});
-	console.log(result);
+function map() {
+  let num = [1, 2, 3, 4, 5];
+  let result = num.map(function (n) {
+    return n + 1;
+  });
+  console.log(result);
 }
 /* map function how is work
 
 copy array crate new array eg ((result array)) is new
+
+*/
+function filter() {
+  let num = [1, 2, 3, 4, 5];
+  let result = num.filter(function (num) {
+    if (num % 2 == 0) {
+      return num;
+    }
+  });
+  console.log(result);
+}
